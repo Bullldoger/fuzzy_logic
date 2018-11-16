1c0a6eccbf94253b4723963f12da9a89edc36808
Roman <kaire1996@gmail.com>
2018-11-16 21:38:54 +0300

    initing repo

--- labs/fuzzy_nums/Arithmetics.py
+++ labs/fuzzy_nums/Arithmetics.py
@@ -8,53 +8,116 @@ class ArithmeticController:
 
     """
 
+    abs_const = 1e-5
+
     def __init__(self):
         pass
 
     @staticmethod
-    def addition(a_1, b_1, a_2, b_2):
+    def addition(a, b, c, d):
         """
 
-        :param a_1:
-        :param b_1:
-        :param a_2:
-        :param b_2:
+        :param a:
+        :param b:
+        :param c:
+        :param d:
         :return:
         """
-        pass
+        return a + c, b + d
 
     @staticmethod
-    def subtraction(a_1, b_1, a_2, b_2):
+    def subtraction(a, b, c, d):
         """
 
-        :param a_1:
-        :param b_1:
-        :param a_2:
-        :param b_2:
+        :param a:
+        :param b:
+        :param c:
+        :param d:
         :return:
         """
-        pass
+        return a - c, b - d
+
 
     @staticmethod
-    def multiplication(a_1, b_1, a_2, b_2):
+    def compute_intervals(a, b, c, d, sign='+'):
         """
 
-        :param a_1:
-        :param b_1:
-        :param a_2:
-        :param b_2:
+        :param a:
+        :param b:
+        :param c:
+        :param d:
         :return:
         """
-        pass
+        result = dict()
+
+        if 0 <= a <= b:
+            if 0 < c <= d:
+                result['/'] = (a / d, b / c)
+                result['*'] = (a * c, b * d)
+            elif c <= 0 <= d:
+                result['*'] = (b * c, b * d)
+            elif c <= d < 0:
+                result['/'] = (b / d, a / c)
+                result['*'] = (b * c, a * d)
+        elif a <= 0 <= b:
+            if 0 < c <= d:
+                result['/'] = (a / d, b / c)
+                result['*'] = (a * c, b * d)
+            elif c <= 0 <= d:
+                result['*'] = (min(a * d, b * c), max(a * c, b * d))
+            elif c <= d < 0:
+                result['/'] = (b / d, a / d)
+                result['*'] = (b * c, a * c)
+        else:
+            if 0 < c <= d:
+                result['/'] = (a / c, b / d)
+                result['*'] = (a * d, b * c)
+            elif c <= 0 < d:
+                result['*'] = (a * d, a * c)
+            elif c <= d < 0:
+                result['/'] = (b / c, b / d)
+                result['*'] = (b * d, a * c)
+
+        return result[sign]
 
     @staticmethod
-    def division(a_1, b_1, a_2, b_2):
+    def multiplication(a, b, c, d):
         """
 
-        :param a_1:
-        :param b_1:
-        :param a_2:
-        :param b_2:
+        :param a:
+        :param b:
+        :param c:
+        :param d:
         :return:
         """
-        pass
+        return ArithmeticController.compute_intervals(a, b, c, d, sign='*')
+
+    @staticmethod
+    def division(a, b, c, d):
+        """
+
+        :param a:
+        :param b:
+        :param c:
+        :param d:
+        :return:
+        """
+
+        if c == 0 or d == 0:
+            if c == 0 and d > 0:
+                left_1, right_1 = ArithmeticController.compute_intervals(a, b, 0 +
+                                                                         ArithmeticController.abs_const, d, sign='/')
+            elif c < 0 and d == 0:
+                left_1, right_1 = ArithmeticController.compute_intervals(a, b, c, 0 +
+                                                                         ArithmeticController.abs_const, sign='/')
+            else:
+                left_1, right_1 = ArithmeticController.compute_intervals(a, b, ArithmeticController.abs_const,
+                                                                         ArithmeticController.abs_const, sign='/')
+        if c <= 0 <= d:
+            _c = 0 - ArithmeticController.abs_const
+            _d = 0 + ArithmeticController.abs_const
+
+            left_1, _ = ArithmeticController.compute_intervals(a, b, c, _c, sign='/')
+            _, right_1 = ArithmeticController.compute_intervals(a, b, _d, d, sign='/')
+
+        return left_1, right_1
--- labs/fuzzy_nums/__init__.py
+++ labs/fuzzy_nums/__init__.py
@@ -5,6 +5,8 @@ import fuzzy_nums
 from fuzzy_nums.MFs import GaussMF, Polygon, TrapMF, TriMF
 from fuzzy_nums.Arithmetics import ArithmeticController
 
+import numpy as np
+
 
 class FuzzyNumber:
     """
@@ -12,6 +14,7 @@ class FuzzyNumber:
     """
     mf_func = None
     arithmetic_controller = None
+    sections = 10
 
     def __init__(self):
         pass
@@ -32,13 +35,46 @@ class FuzzyNumber:
         """
         self.arithmetic_controller = arithmetic_controller
 
+    def get_alpha_cut(self, alpha=1):
+        """
+
+        :param alpha:
+        :return:
+        """
+        return self.mf_func.alpha_cut(alpha=alpha)
+
+    def handle_operation(self, other, operation='+'):
+        """
+
+        :param other:
+        :param operation:
+        :return:
+        """
+
+        for alpha in np.linspace(0, 1, self.sections):
+            a_1, b_1 = self.get_alpha_cut(alpha=alpha)
+            a_2, b_2 = other.get_alpha_cut(alpha=alpha)
+
+            a, b = 0, 0
+
+            if operation == '+':
+                a, b = self.arithmetic_controller.addition(a_1, b_1, a_2, b_2)
+            elif operation == '-':
+                a, b = self.arithmetic_controller.subtraction(a_1, b_1, a_2, b_2)
+            elif operation == '*':
+                a, b = self.arithmetic_controller.multiplication(a_1, b_1, a_2, b_2)
+            elif operation == '/':
+                a, b = self.arithmetic_controller.division(a_1, b_1, a_2, b_2)
+
+        pass
+
     def __add__(self, other):
         """
 
         :param fuzzy_num:
         :return:
         """
-        pass
+        return self.handle_operation(other, operation='+')
 
     def __sub__(self, other):
         """
@@ -46,7 +82,7 @@ class FuzzyNumber:
         :param other:
         :return:
         """
-        pass
+        return self.handle_operation(other, operation='-')
 
     def __mul__(self, other):
         """
@@ -54,7 +90,7 @@ class FuzzyNumber:
         :param other:
         :return:
         """
-        pass
+        return self.handle_operation(other, operation='*')
 
     def __truediv__(self, other):
         """
@@ -62,4 +98,4 @@ class FuzzyNumber:
         :param other:
         :return:
         """
-        pass
+        return self.handle_operation(other, operation='/')
