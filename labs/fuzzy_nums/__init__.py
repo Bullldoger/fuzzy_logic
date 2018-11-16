bb5cf9b83b5b62700a1596b81a430279a5b7294c
Roman <kaire1996@gmail.com>
2018-11-16 19:30:02 +0300

    initing repo

---
+++ labs/fuzzy_nums/__init__.py
@@ -0,0 +1,185 @@
+"""
+
+"""
+
+import numpy as np
+
+
+class TriMF:
+    """
+
+    """
+
+    def __init__(self, a, b, c):
+        """
+        f(x|a, b, c) = max(min((x − a) / (b − a), (c − x) / (c − b)), 0)
+        :return:
+        :param a:
+        :param b:
+        :param c:
+        :return:
+        """
+
+    def f(self, x):
+        """
+
+        :param x:
+        :return:
+        """
+        pass
+
+    def alpha_cut(self, alpha=1):
+        """
+
+        :param alpha:
+        :return:
+        """
+        pass
+
+
+class TrapMF:
+    """
+
+    """
+
+    def __init__(self, a, b, c, d):
+        """
+        f(x|a, b, c, d) = max(min( (x − a) / (b − a), 1, (d − x) / (d − c)), 0)
+        :param a:
+        :param b:
+        :param c:
+        :param d:
+        :return:
+        """
+
+    def f(self, x):
+        """
+
+        :param x:
+        :return:
+        """
+        pass
+
+    def alpha_cut(self, alpha=1):
+        """
+
+        :param alpha:
+        :return:
+        """
+        pass
+
+
+class GaussMF:
+    """
+
+    """
+
+    def __init__(self, c, sig):
+        """
+        f(x|c, sig) = exp{-(x - c) ** 2 / (s * sig ** 2)}
+        :param c:
+        :param sig:
+        :return:
+        """
+
+    def f(self, x):
+        """
+
+        :param x:
+        :return:
+        """
+    pass
+
+    def alpha_cut(self, alpha=1):
+        """
+
+        :param alpha:
+        :return:
+        """
+        pass
+
+
+class ArithmeticController:
+    """
+
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def addition(a_1, b_1, a_2, b_2):
+        """
+
+        :param a_1:
+        :param b_1:
+        :param a_2:
+        :param b_2:
+        :return:
+        """
+        pass
+
+    @staticmethod
+    def subtraction(a_1, b_1, a_2, b_2):
+        """
+
+        :param a_1:
+        :param b_1:
+        :param a_2:
+        :param b_2:
+        :return:
+        """
+        pass
+
+    @staticmethod
+    def multiplication(a_1, b_1, a_2, b_2):
+        """
+
+        :param a_1:
+        :param b_1:
+        :param a_2:
+        :param b_2:
+        :return:
+        """
+        pass
+
+    @staticmethod
+    def division(a_1, b_1, a_2, b_2):
+        """
+
+        :param a_1:
+        :param b_1:
+        :param a_2:
+        :param b_2:
+        :return:
+        """
+        pass
+
+
+class FuzzyNumber:
+    """
+
+    """
+    mf_func = None
+    arithmetic_controller = None
+
+    def __init__(self):
+        pass
+
+    def set_mf(self, mf_func):
+        """
+
+        :param mf_func:
+        :return:
+        """
+        self.mf_func = mf_func
+
+    def set_arithmetic_controller(self, arithmetic_controller):
+        """
+
+        :param arithmetic_controller:
+        :return:
+        """
+        self.arithmetic_controller = arithmetic_controller
+
+
